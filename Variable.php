75c76332a2a916ea8b99c8ce9d7a088015797f67
JohnPaul005 <johnpaulespinosa05@gmail.com>
2022-10-12 21:35:02 +0800

    review php

---
+++ Variable.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+        /* Example of valid variable naming   
+            $color = '';
+            $_color = '';
+            $1color = ''; //invalid variable
+            $color1 = '';
+            $my color = '';//invalid variable
+        */
+
+        /* Data Types
+            Integers
+            Doubles
+            Booleans
+            NULL
+            String
+            Arrays
+            Objects
+            Resources
+        */
+
+        /* Variables
+            $age = 20; 
+            $pi = 3.1416;
+            $Status = true;
+            $error = null;
+            $name = "John Paul";
+
+            echo $age . '<br>';
+            echo $pi . '<br>';
+            echo $Status . '<br>';
+            echo $error . '<br>';
+            echo $name . '<br>';
+        */
+
+        /* constant 
+            to use constant use the function "define" natatangapin ng 2 arguments
+            first argument the name of constant with coma
+            second argument will be the value of the constant
+            
+            define("SITE_URL", "http://test.test/");
+            echo SITE_URL;
+        */
+    ?>
+</body>
+</html>
---
+++ demo_echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        //  $name = "John Paul";
+        // // echo $name;
+        // // echo 'Hello ' . $name;
+        // echo "$name";
+
+        // echo '<h1 align="Center">John Paul</h1>';
+
+            // echo 'Hello ' , $name, ' how are you?';
+            // print 'Hellow World!';
+    ?>
+
+</body>
+</html>
