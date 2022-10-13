56b4cb6806d8b7d96ae1781f50036f7854df4322
JohnPaul005 <johnpaulespinosa05@gmail.com>
2022-10-13 21:59:05 +0800

    done in operators demo

---
+++ operators_demo.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        
+       
+        
+        $x = 10;
+        $y = 4;
+        $z = "10";
+        
+         //Arithmetic Operators
+        /*
+            echo $x + $y, '<br>'; //14
+            echo $x - $y, '<br>'; //6
+            echo $x * $y, '<br>'; //40
+            echo $x / $y, '<br>'; //2.5
+            echo $x % $y, '<br>'; //2
+            echo $x ** $y, '<br>'; //10000
+        */
+
+        //Assignment Operators
+        /*
+            echo $x += $y, '<br>';
+            echo $x -= $y, '<br>';
+            echo $x *= $y, '<br>';
+            echo $x /= $y, '<br>';
+            echo $x %= $y, '<br>';
+        */
+        
+        //Comparison Operators
+        /*
+            echo $x == $z, '<br>';
+            echo $x === $z, '<br>';
+            echo $x != $y, '<br>';
+            echo $x <> $z, '<br>';
+            echo $x !== $z, '<br>';
+            echo $x < $y, '<br>';
+            echo $x > $y, '<br>';
+        */
+
+        //Incrementing and decrementing Operators
+        /*
+            echo $x++, '<br>';
+            echo ++$y, '<br>';
+            echo $x--, '<br>';
+            echo --$x, '<br>';
+        */
+
+        //Logical Operators
+        /*
+            $w = 10;
+            echo ($x == $w) and ($x > $y), '<br>';
+            echo ($x == $w) && ($x < $y), '<br>';
+            echo ($x == $w) or ($x > $y), '<br>';
+            echo ($x == $w) || ($x < $y), '<br>';
+            echo ($x == $w) xor ($x > $y), '<br>';
+            echo ($x == $w) xor ($x < $y), '<br>';
+        */
+
+
+        //Conditional Operators
+        /*
+            echo ($x == $y ? 'Same Value' : 'Unlike Value'), '<br>';
+            echo ($x > $y ? 'Greater Value' : 'Less Value'), '<br>';
+            echo ($x < $y ? 'Greater Value' : 'Less Value'), '<br>';
+        */
+    
+    ?>
+</body>
+</html>
