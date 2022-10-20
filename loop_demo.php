4f029219a37c9f0a5b9423560bdfa4b36b41db99
JohnPaul005 <johnpaulespinosa05@gmail.com>
2022-10-20 19:40:45 +0800

    looping

---
+++ loop_demo.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        /*tanong muna bago pasok
+        $i = 2;
+        while($i < 11){
+            echo $i . ' ';
+            $i++;
+        }
+        */
+
+        /* pasok muna bago tanong
+        $i = 1;
+        do {
+            echo $i . ' ';
+            $i++;
+        } while($i < 11);
+        */
+        
+        /*
+        for($i = 1; $i < 11; $i++)
+            echo $i . ' ';
+        */
+    ?>
+</body>
+</html>
